3424f594ffd0da56ed9c644c4adb49c2fb20e35d
Nayed SAID ALI <nayed.saidali@gmail.com>
2016-01-03 19:17:06 +0100

    home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Home</h1>
+    <h1>Image Subreddit Gallery</h1>
+    
+    <p v-for="link in links">
+        <a :href="'r/' + link">r/{{link}}</a>
+    </p>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                links: ['pics', 'wallpaper', 'reactiongifs', 'wallpapers', 'aww', 'cats', 'foxes', 'images', 'memes']
+            }
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style>
+</style>
