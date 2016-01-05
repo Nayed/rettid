8ef7b1f48e67e48bf9a7029549ecf92c72d4ad81
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-01-05 17:25:41 +0100

    testing at school trololo

--- src/components/R.vue
+++ src/components/R.vue
@@ -1,11 +1,16 @@
 <template>
     <h1>Image Subreddit Gallery</h1>
     {{sub}}
-    {{getImg(sub)}}</template>
+    <br/>
+    <br/>
+    <br/>
+    {{//getImg(sub)}}
+    {{testy()}}
+</template>
 
 <script>
 
-    import http from 'http'
+    import VerEx from 'verbal-expressions'
 
     export default {
         data() {
@@ -22,8 +27,32 @@
                 })
                 .error(err => console.log(err))
 
-
                 return JSON.stringify(this.img)
+            },
+
+            testy() {
+                let content = JSON.stringify(this.img)
+
+                let testhing = 'ame":"t3_3zfx93","created":1451960437,"url":"http://i.imgur.com/9ZgDaaV.jpg","author_flair_'
+
+                /*let truthy = /(https?:\/\/.*\.(?:png|jpg|gif))/i
+                console.log(`regex is: ${truthy} and typeof ${typeof truthy}`)*/
+
+                let truthy = VerEx()
+                                .startOfLine()
+                                .then("http://i.imgur.com/")
+                                .anythingBut(" ")
+                                .then("jpg")
+                                .endOfLine()
+
+                truthy = /(http:\/\/i\.imgur.*jpg)/g
+                console.log(truthy)
+
+                let real = testhing.match(truthy)
+
+                console.log(real)
+
+                return testhing
             }
         }
     }
